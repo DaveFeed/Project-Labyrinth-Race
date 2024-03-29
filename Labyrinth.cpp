93144ae71c966dd9449f01625f0a0acaa75be18a
Mike Oxlong <karkanduck@gmail.com>
2024-03-29 23:45:23 +0400

    Changed matrix completely, now it's thinner, looking better.

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -5,7 +5,7 @@
 Labyrinth::Labyrinth(unsigned height, unsigned width):
 height(height)
 ,width(width)
-,data(width, std::vector<int>(height, 0))
+,labyrinth(width * 2 + 1, std::vector<int>(height * 2 + 1, 1))
 {
     srand(time(NULL));
     std::cout << "Successfully initialized labyrinth!\n";
@@ -13,70 +13,48 @@ height(height)
 
 void Labyrinth::init()
 {
-    std::stack<std::pair<int, int>> st;             //for dfs
-    std::vector<char> used(height * width, false);
+    std::stack<std::pair<int, int>> st;
 
-    int x = (rand() % width);                       //where to start dfs
-    int y = (rand() % height);
-
-    auto index = [&](int x, int y) {                //lambda function that gets (x, y) coords and returns its index
-        return (y * width) + x;
-    };
+    int x = (rand() % 2 * width);
+    if(x % 2 == 0) {
+        ++x;
+    }                  
+    int y = (rand() % 2 * height);
+    if(y % 2 == 0) {
+        ++y;
+    }
 
     st.push(std::make_pair(x, y));
 
     while(!st.empty()) {
-        used[index(x, y)] = true;
-        //checking available neighbors
-        std::vector<int> neighs;
+        labyrinth[x][y] = 0;
+        std::vector<std::pair<int, int>> neighs;
 
-        if(x > 0) {                         //has left neighbor
-            if(!used[index(x - 1, y)])      //the left neighbor has not been visited
-                neighs.push_back(PATHS::LEFT);
+        if(x > 1) {                         
+            if(labyrinth[x - 2][y] == 1)      
+                neighs.push_back(std::make_pair(x - 2, y));
         }
-        if(x < width - 1) {                 //has right neighbor
-            if(!used[index(x + 1, y)])      //the right neighbor has not been visited
-                neighs.push_back(PATHS::RIGHT);
+        if(x < 2 * width - 1) {                
+            if(labyrinth[x + 2][y] == 1)      
+                neighs.push_back(std::make_pair(x + 2, y));
         }
-        if(y > 0) {                         //has upper neighbor
-            if(!used[index(x, y - 1)])      //the upper neighbor has not been visited
-                neighs.push_back(PATHS::UP);
+        if(y > 1) {                         
+            if(labyrinth[x][y - 2] == 1)      
+                neighs.push_back(std::make_pair(x, y - 2));
         }
-        if(y < height - 1) {                //has bottom neighbor
-            if(!used[index(x, y + 1)])      //the bottom neighbor has not been visited
-                neighs.push_back(PATHS::DOWN);
+        if(y < 2 * height - 1) {                
+            if(labyrinth[x][y + 2] == 1)      
+                neighs.push_back(std::make_pair(x, y + 2));
         }
 
-        // randomly choosing one neighbor, if no available neighbors then go back
         if(!neighs.empty()) {
-            int neighbor_dir = neighs[rand() % neighs.size()];
-
-            switch(neighbor_dir) {
-                case PATHS::LEFT:
-                    data[x][y] |= PATHS::LEFT;
-                    --x;
-                    data[x][y] |= PATHS::RIGHT;
-                    st.push(std::make_pair(x, y));
-                    break;
-                case PATHS::RIGHT:
-                    data[x][y] |= PATHS::RIGHT;
-                    ++x;
-                    data[x][y] |= PATHS::LEFT;
-                    st.push(std::make_pair(x, y));
-                    break;
-                case PATHS::UP:
-                    data[x][y] |= PATHS::UP;
-                    --y;
-                    data[x][y] |= PATHS::DOWN;
-                    st.push(std::make_pair(x, y));
-                    break;
-                case PATHS::DOWN:
-                    data[x][y] |= PATHS::DOWN;
-                    ++y;
-                    data[x][y] |= PATHS::UP;
-                    st.push(std::make_pair(x, y));
-                    break;
-            }
+            int neighbor = rand() % neighs.size();
+            int new_x = neighs[neighbor].first;
+            int new_y = neighs[neighbor].second;
+            labyrinth[(x + new_x) / 2][(y + new_y) / 2] = 0;
+            x = new_x;
+            y = new_y;
+            st.push(std::make_pair(x, y));
         }
         else {
             st.pop();
@@ -86,64 +64,71 @@ void Labyrinth::init()
             }
         }
     }
+
+    for(int i = 2; i < width * 2; i += 2) {
+        for(int j = 2; j < height * 2; j += 2) {
+            labyrinth[i][j] = 2;                        //on odd x odd places are wall intersections(number 2)
+        }
+    }
     
+    std::cout << "Labyrinth created, now adding loops\n";
+
     // CREATING LOOPS
     for(int i = 0; i < 100; ++i) {
         while(true) {
-            int x = rand() % width;
-            int y = rand() % height;
-
-            int available_path = 0b1111;
-            if(x == 0)
-                available_path -= PATHS::LEFT;
-            if(x == width - 1)
-                available_path -= PATHS::RIGHT;
-            if(y == 0)
-                available_path -= PATHS::UP;
-            if(y == height - 1)
-                available_path -= PATHS::DOWN;
-            
-            //leave bits that are 1 only in available_path
-            available_path |= data[x][y];
-            available_path -= data[x][y];
-            
-            if((available_path & 0b1111) != 0) {
-                bool cond = false;
-                while(!cond) {
-                    int num = rand() % 4;
-                    switch(num) {
-                        case 0:
-                            if((available_path & PATHS::DOWN) != 0) {
-                                data[x][y] |= PATHS::DOWN;
-                                data[x][y+1] |= PATHS::UP;
-                                cond = true;
-                            }
-                            break;
-                        case 1:
-                            if((available_path & PATHS::UP) != 0) {
-                                data[x][y] |= PATHS::UP;
-                                data[x][y-1] |= PATHS::DOWN;
-                                cond = true;
-                            }
-                            break;
-                        case 2:
-                            if((available_path & PATHS::LEFT) != 0) {
-                                data[x][y] |= PATHS::LEFT;
-                                data[x-1][y] |= PATHS::RIGHT;
-                                cond = true;
-                            }
-                            break;
-                        case 3:
-                            if((available_path & PATHS::RIGHT) != 0) {
-                                data[x][y] |= PATHS::RIGHT;
-                                data[x+1][y] |= PATHS::LEFT;
-                                cond = true;
-                            }
-                            break;
-                    }
+            int x = rand() % (2 * width - 1) + 1;
+            int y = rand() % (2 * height -1) + 1;
+            std::cout << "x = " << x << " y = " << y << std::endl;
+            if(x % 2 == 0) {
+                while(y % 2 == 0) {
+                    y = y + 1;
                 }
+            } else {
+                if(y % 2 != 0) {
+                    y = (y + 1) % (2 * width - 1);
+                }
+            }
+            std::cout << "new x = " << x << " new y = " << y << std::endl;
+
+
+            if(labyrinth[x][y] == 1) {
+                labyrinth[x][y] = 0;
                 break;
             }
         }
+        std::cout << "wall created\n";
     }
+    std::cout << "Loops added\n";
+}
+
+void Labyrinth::draw()
+{
+    int wall = 0;
+
+    //i shows the rows
+    for(int i = 0; i < 2 * height + 1; ++i) {
+        //j shows the columns
+        for(int j = 0; j < 2 * width + 1; ++j) {
+            if(labyrinth[i][j] == 0) {
+                std::cout << ' ';
+            }
+            else if(labyrinth[i][j] == 2) {
+                std::cout << '+';
+            }
+            else {
+                if(j == 0 || j == 2 * width || i % 2 != 0) {
+                    std::cout << '|';
+                    ++wall;
+                }
+                else {
+                    std::cout << '-';
+                    ++wall;
+                }
+            }
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << wall << std::endl;
 }
